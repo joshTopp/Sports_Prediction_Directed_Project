62f8a81d4f248962cb15574906e424036f928b05
D1Dika <133713297+MrStarshy@users.noreply.github.com>
2026-07-13 20:38:29 -0500

    Update and rename model_service,py to model_service.py
    Added backend risk level logic.

--- backend/model_service.py
+++ backend/model_service.py
@@ -14,16 +14,30 @@ class ModelService:
     def __init__(self):
         self.model = joblib.load("models/game_model.joblib")
 
-    def predict_game(self, data):
+    def get_risk_level(self, confidence):
+        confidence_percent = confidence * 100
 
+        if confidence_percent >= 80:
+            return "Low"
+        elif confidence_percent >= 60:
+            return "Medium"
+        else:
+            return "High"
+
+    def predict_game(self, data):
         df = pd.DataFrame([data])
 
         prediction = self.model.predict(df)[0]
         probabilities = self.model.predict_proba(df)[0]
 
+        confidence = max(probabilities)
+        confidence_score = round(confidence * 100, 2)
+        risk_level = self.get_risk_level(confidence)
+
         return {
             "winner": "home" if prediction == 1 else "away",
-            "confidence": max(probabilities)
+            "confidence_score": confidence_score,
+            "risk_level": risk_level
         }
 
 
